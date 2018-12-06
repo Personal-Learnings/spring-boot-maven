cd824614e87b5183171588ef9b4851236e6a3a84
Madanraj Venkatesan <mjyashhome@Madanrajs-MacBook-Air.local>
2018-12-05 18:56:14 -0800

    Spring Boot Project Setup

---
+++ src/main/java/com/learning/spring/boot/maven/SpringBootMavenApplication.java
@@ -0,0 +1,12 @@
+package com.learning.spring.boot.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApplication.class, args);
+	}
+}
---
+++ src/main/java/com/learning/spring/boot/maven/controller/ExpenseController.java
@@ -0,0 +1,9 @@
+package com.learning.spring.boot.maven.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+
+
+}
